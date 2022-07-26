d56cd6f673851aea36ffd6901f765a7411e7d613
Ahmed-2020-98 <ahmedgamalmansour11@gmail.com>
2022-07-26 21:23:19 +0200

    done day 3 react

--- src/products/products.js
+++ src/products/products.js
@@ -2,14 +2,14 @@ import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import axiosInstance from './../axiosConfig/instance';
-
+import axiosInstance from "./../axiosConfig/instance";
+import "./products.css";
 const Products = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    
-    axiosInstance.get("movie/popular")
+    axiosInstance
+      .get("movie/popular")
       .then((res) => {
         console.log(res.data.results);
         setProducts(res.data.results);
@@ -18,23 +18,33 @@ const Products = () => {
         console.log(err);
       });
 
-
-
     //   axios.post("url","data",{})
   }, []);
 
   return (
     <>
-      <ul>
-        {products.map((product) => {
+      {/* {products.map((product) => {
           return (
             <Link to={`/details/${product.id}`} key={product.id}>
-              <li>{product.title}</li>
-              
+               <h1 className="n">{product.title}</h1>
+                <img src={`https://image.tmdb.org/t/p/w500/${product.backdrop_path}`} />
             </Link>
           );
-        })}
-      </ul>
+        })} */}
+      <div className="container">
+        <div className="cart my-5">
+          {products.map((product) => {
+            return (
+              <>
+                <div className="item">
+                  <img src={`https://image.tmdb.org/t/p/w500/${product.backdrop_path}`}/>
+                  <p>{product.title}</p>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
